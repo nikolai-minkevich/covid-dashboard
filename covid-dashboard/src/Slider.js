bc52d844b21df098bc9e37e3f9754937f9b2ff7e
NastyaVasenina <amedeo_@mail.ru>
2020-12-17 00:10:55 +0300

    fix: fix return of slider

--- covid-dashboard/src/Slider.js
+++ covid-dashboard/src/Slider.js
@@ -1,12 +1,13 @@
 import create from "./create";
 class Slider{
-    constructor(nameOfItem){
+    constructor(nameOfItem/*, className*/){
         this.nameOfItem = nameOfItem;
+        /*this.className = className;*/
         return this.generateLayout();
     }
     generateLayout(){
         const nameOfItem = create('div', 'slider_nameOfItem', `${this.nameOfItem}`);
-        create("div", "slider_container", [
+        return create("div", "slider_container", [
             create("div", 'slider_leftArrow', "<"),
             nameOfItem,
             create("div", "slider_rightArrow", ">")
