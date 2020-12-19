63316d7e21deba9ec118e7c9e5dcd6c5b653765e
NastyaVasenina <amedeo_@mail.ru>
2020-12-19 18:22:30 +0300

    feat: add items for country table

--- covid-dashboard/src/CountryStatistic.js
+++ covid-dashboard/src/CountryStatistic.js
@@ -2,62 +2,137 @@ import create from "./create";
 import Slider from "./Slider";
 import StatisticItem from "./StatisticItem";
 
-
 //демолист, надо переделать на результат из апи
 const demoListExample = [
-    {
-      country: "Russia",
-      score: 2000
-    },
-    {
-      country: "Russia",
-      score: 2000
-    },
-    {
-      country: "Russia",
-      score: 2000
-    },
-    {
-      country: "Russia",
-      score: 2000
-    },
-    {
-      country: "Russia",
-      score: 2000
-    },
-    {
-      country: "Russia",
-      score: 2000
-    },
-    {
-      country: "Russia",
-      score: 2000
-    }
-  ]
-  
+  {
+    country: "Russia",
+    score: 2000,
+  },
+  {
+    country: "Russia",
+    score: 2000,
+  },
+  {
+    country: "Russia",
+    score: 2000,
+  },
+  {
+    country: "Russia",
+    score: 2000,
+  },
+  {
+    country: "Russia",
+    score: 2000,
+  },
+  {
+    country: "Russia",
+    score: 2000,
+  },
+  {
+    country: "Russia",
+    score: 2000,
+  },
+];
+
 class CountryStatistic {
   constructor(countries) {
+    this.choisenItem = 0;
     this.countries = countries;
     return this.generateLayout();
   }
   generateLayout() {
+    const demoListItems = [
+      "total cases",
+      "total deaths",
+      "total recovered",
+      "total cases per 100000",
+      "total deaths per 100000",
+      "total recovered per 100000",
+      "today cases",
+      "today deaths",
+      "today recovered",
+      "today cases per 100000",
+      "today deaths per 100000",
+      "today recovered per 100000",
+    ];
     const countries = this.countries;
     console.log(this.countries, "countries");
-    const demoList =[];
+    const demoList = [];
+    for (let i = 0; i <demoListItems.length; i += 1){
+      demoList.push([])
+    }
     countries.map((item) => {
-      demoList.push({
+      demoList[0].push({
         country: item.country,
-        score: item.cases
-      })
-    })
-    demoList.sort((a, b)=>a.score < b.score ? 1 : -1)
-    const countryStatistic = new StatisticItem("Russia Cases", demoList, true);
-    const slider = new Slider("жужужу");
-    const countryStatisticContainer = create("div", "statistic_container__prime countryStatistic_container__prime", [
-        countryStatistic,
-      slider
-    ]);
-    return countryStatisticContainer
+        score: item.cases,
+      });
+      demoList[1].push({
+        country: item.country,
+        score: item.deaths,
+      });
+      demoList[2].push({
+        country: item.country,
+        score: item.recovered,
+      });
+
+      demoList[3].push({
+        country: item.country,
+        score: item.casesPerOneMillion * 10,
+      });
+
+
+      demoList[4].push({
+        country: item.country,
+        score: item.deathsPerOneMillion * 10,
+      });
+
+
+      demoList[5].push({
+        country: item.country,
+        score: item.recoveredPerOneMillion * 10,
+      });
+
+
+      demoList[6].push({
+        country: item.country,
+        score: item.todayCases,
+      });
+      demoList[7].push({
+        country: item.country,
+        score: item.todayDeaths,
+      });
+      demoList[8].push({
+        country: item.country,
+        score: item.todayRecovered,
+      });
+      demoList[9].push({
+        country: item.country,
+        score: (item.todayCases * 100000)/item.population,
+      });
+      demoList[10].push({
+        country: item.country,
+        score: (item.todayDeaths* 100000)/item.population,
+      });
+      demoList[11].push({
+        country: item.country,
+        score: (item.todayRecovered* 100000)/item.population,
+      });
+    });
+    for(let i =0; i<demoList.length; i+=1){
+      demoList[i].sort((a, b) => (a.score < b.score ? 1 : -1));
+    }
+    const countryStatistic = new StatisticItem("Russia Cases", demoList[this.choisenItem], true);
+    const slider = new Slider(
+      `${demoListItems[this.choisenItem]}`,
+      "countryStatistic__left",
+      "countryStatistic__right"
+    );
+    const countryStatisticContainer = create(
+      "div",
+      "statistic_container__prime countryStatistic_container__prime",
+      [countryStatistic, slider]
+    );
+    return countryStatisticContainer;
   }
 }
 export default CountryStatistic;
--- covid-dashboard/src/StatisticTable.js
+++ covid-dashboard/src/StatisticTable.js
@@ -1,49 +1,6 @@
 import create from "./create";
 import Slider from "./Slider";
 import StatisticItem from "./StatisticItem";
-//демолист, надо переделать на результат из апи
-const demoListExample = [
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-  {
-    country: "Russia",
-    score: 2000,
-  },
-];
 
 class StatisticTable {
   constructor(results) {
@@ -52,7 +9,12 @@ class StatisticTable {
     return this.generateLayout();
   }
   generateLayout() {
-    const demoListItems = ['total', 'today', 'total per 100000', 'today per 100000'];
+    const demoListItems = [
+      "total cases / deaths / recovered",
+      "today cases / deaths / recovered",
+      "total cases / deaths / recovered per 100000",
+      "today cases / deaths / recovered per 100000",
+    ];
     console.log(this.results);
     const totalCases = this.results.cases;
     const totalDeaths = this.results.deaths;
@@ -70,10 +32,20 @@ class StatisticTable {
     const todayDeaths = this.results.todayDeaths;
     const todayRecovered = this.results.todayRecovered;
     const today = [todayCases, todayDeaths, todayRecovered];
-    const todayCasesPer100000 = Math.round(this.results.todayCases/(totalCases / totalCasesPer100000));
-    const todayDeathsPer100000 = Math.round(this.results.todayDeaths/(totalDeaths / totalDeathsPer100000));
-    const todayRecoveredPer100000 = Math.round(this.results.todayRecovered/(totalRecovered / totalRecoveredPer100000));
-    const todayPer100000 = [todayCasesPer100000, todayDeathsPer100000, todayRecoveredPer100000]
+    const todayCasesPer100000 = Math.round(
+      this.results.todayCases / (totalCases / totalCasesPer100000)
+    );
+    const todayDeathsPer100000 = Math.round(
+      this.results.todayDeaths / (totalDeaths / totalDeathsPer100000)
+    );
+    const todayRecoveredPer100000 = Math.round(
+      this.results.todayRecovered / (totalRecovered / totalRecoveredPer100000)
+    );
+    const todayPer100000 = [
+      todayCasesPer100000,
+      todayDeathsPer100000,
+      todayRecoveredPer100000,
+    ];
     const demoList = [total, today, totalPer100000, todayPer100000];
     const casesContainer = new StatisticItem(
       "Cases",
@@ -90,7 +62,11 @@ class StatisticTable {
       [demoList[this.choisenItem][2]],
       false
     );
-    const slider = new Slider(`${demoListItems[this.choisenItem]}`, 'statisticTable__left', 'statisticTable__right');
+    const slider = new Slider(
+      `${demoListItems[this.choisenItem]}`,
+      "statisticTable__left",
+      "statisticTable__right"
+    );
     //console.log(slider.classList);
     const statisticContainer = create("div", "statistic_container__prime", [
       casesContainer,
@@ -100,8 +76,6 @@ class StatisticTable {
     ]);
     return statisticContainer;
   }
-  changeChoisenItem(){
-    
-  }
+  changeChoisenItem() {}
 }
 export default StatisticTable;
