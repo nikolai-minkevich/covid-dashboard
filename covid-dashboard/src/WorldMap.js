42b03a73ae4e3ac8d8fd6a975cd22ba38b07ca04
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2020-12-20 20:25:43 +0300

    feat: fit map for selected country

--- covid-dashboard/src/WorldMap.js
+++ covid-dashboard/src/WorldMap.js
@@ -1,5 +1,6 @@
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+let convert = require('xml-js')
 
 //import countries from '../assets/data/countries.json'
 
@@ -9,9 +10,11 @@ class WorldMap {
 
     constructor() {
         this.worldMap = '';
+        this.geonamesAPI = new GeonamesAPI();
         //this.countries = countries
         // this.getCountryList();
-        console.log(this.countries);
+
+        // console.log(this.countries);
     }
 
     generateLayout() {
@@ -23,21 +26,22 @@ class WorldMap {
             document.querySelector('.mainContent_container').firstChild.after(this.worldMap);
             this.init();
         }, 1500)
-    }
-
-    getCountryList() {
-        return fetch('../assets/data/countries.json')
-            .then(response => {
-                console.log('countries', response);
-                if (!response.ok) throw Error(response.statusText);
-                this.countries = response.json();
-            })
-            .catch(error => console.log(error));
 
     }
+
+    // getCountryList() {
+    //     return fetch('../assets/data/countries.json')
+    //         .then(response => {
+    //             // console.log('countries', response);
+    //             if (!response.ok) throw Error(response.statusText);
+    //             this.countries = response.json();
+    //         })
+    //         .catch(error => console.log(error));
+
+    // }
 
     init() {
-        let mymap = L.map(this.worldMap.id).setView([30.0, 60.0], 2);
+        this.mymap = L.map(this.worldMap.id).setView([30.0, 60.0], 2);
 
         L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`, {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -46,21 +50,58 @@ class WorldMap {
             tileSize: 512,
             zoomOffset: -1,
             accessToken: 'pk.eyJ1Ijoibm1pbmtldmljaCIsImEiOiJjazVqeGVnYjEwN2trM29ybWtrdDBvOXFzIn0.0y70HLurlAEtyMY-ahO4CA'
-        }).addTo(mymap);
+        }).addTo(this.mymap);
 
-        const geonamesAPI = new GeonamesAPI();
+        // Get name (and stats) of selected country
+
+        let self = this;
         function onMapClick(e) {
-            geonamesAPI.getCountryName(e.latlng.lat, e.latlng.lng).then(data => {
-                document.querySelector('.mainContent_container').setAttribute('data-country', data);
-                L.popup()
-                    .setLatLng(e.latlng)
-                    .setContent(data)
-                    .openOn(mymap);
+            console.log('latlng', e.latlng);
+            self.geonamesAPI.getCountryName(e.latlng.lat, e.latlng.lng).then(countryCode => {
+                document.querySelector('.mainContent_container').setAttribute('data-country', countryCode);
+                self.showCountryBounds(countryCode)
+
+                // L.popup()
+                //     .setLatLng(e.latlng)
+                //     .setContent(data)
+                //     .openOn(this.mymap);
             });
 
         }
-        mymap.on('click', onMapClick);
+        this.mymap.on('click', onMapClick);
+
+        // Add rounds with statistic
+
+
+
     }
+    /**
+     * Fit map bounds to selected country
+     * @param {string} countryCode - iso2 country code
+     */
+    showCountryBounds(countryCode) {
+        this.geonamesAPI.getCountryInfo(countryCode).then(countryInfoXML => {
+            //console.log(convert.xml2json(countryInfoXML, { "compact": true }));
+            let countryInfo = JSON.parse(convert.xml2json(countryInfoXML, { "compact": true })).geonames.country;
+            // console.log("countryInfo", countryInfo);
+            let bounds = null;
+            // Russia so great and we need use a special trick for fit the map for show Russia
+            if (countryInfo.countryCode._text === 'RU') {
+                bounds = [
+                    [Math.abs(parseFloat(countryInfo.north._text)), Math.abs(parseFloat(countryInfo.west._text))],
+                    [Math.abs(parseFloat(countryInfo.south._text)), Math.abs(parseFloat(countryInfo.east._text))]
+                ];
+            } else {
+                bounds = [
+                    [parseFloat(countryInfo.north._text), parseFloat(countryInfo.west._text)],
+                    [parseFloat(countryInfo.south._text), parseFloat(countryInfo.east._text)]
+                ];
+            }
+            this.mymap.fitBounds(bounds);
+        });
+    }
+
+
 
 }
 
