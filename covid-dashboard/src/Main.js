1e74a0206754195cacca77855287b536c4848f00
NastyaVasenina <amedeo_@mail.ru>
2020-12-20 17:34:31 +0300

    feat: add search by countryCode

--- covid-dashboard/src/Main.js
+++ covid-dashboard/src/Main.js
@@ -16,6 +16,7 @@ class Main {
       .append(create("main", "mainContent_container"));
     const covid19API = new Covid19API();
     covid19API.getCountries().then((data) => {
+      this.countriesData= data;
       countryStatistic.generateLayout(data);
       this.setupListeners();
     });
@@ -28,12 +29,10 @@ class Main {
     document.querySelectorAll(".countryStatistic_demo_item").forEach((item) => {
       item.addEventListener("click", (e) => {
         if(document.querySelector(".countryStatistic_demo_item__chosen")){
-          console.log(document.querySelector(".countryStatistic_demo_item__chosen"));
           document.querySelector(".countryStatistic_demo_item__chosen").classList.remove("countryStatistic_demo_item__chosen")
         }
         e.stopPropagation();
         const country = e.target.closest(".countryStatistic_demo_item");
-
         country.classList.add("countryStatistic_demo_item__chosen")
         if (!country) return;
         this.choseCountry = country.querySelector(".demo_country").innerHTML;
@@ -47,7 +46,13 @@ class Main {
     // Add mutation observer for main
     let mainContainerObserver = new MutationObserver(mutationRecords => {
       if (mutationRecords[0].attributeName === 'data-country') {
-        console.log(document.querySelector('.mainContent_container').dataset.country);
+        const countryCode = document.querySelector('.mainContent_container').dataset.country
+        console.log("countryCode", countryCode);
+          this.countriesData.map((item)=>{
+            if (item.countryInfo.iso2 == countryCode.trim()){
+              console.log("item.countryInfo.iso2", item.countryInfo.iso2, countryCode);
+            }
+        });
       }
     });
     mainContainerObserver.observe(document.querySelector('.mainContent_container'), {
