4744c5dff0a23d25f150cc6b33369ca05a06fff6
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2020-12-21 21:51:17 +0300

    feat: add methods for get historical data

--- covid-dashboard/src/Covid19API.js
+++ covid-dashboard/src/Covid19API.js
@@ -50,6 +50,63 @@ class Covid19API {
         .catch(error => console.log(error));
     }
   }
+
+  /**
+   * Get COVID-19 time series data for all countries and their provinces.
+   * @param {string} lastdays - Number of days to return. Use 'all' for the full data set (e.g. 15, all, 24).
+   */
+  async getHistorical(lastdays = 30) {
+    if (arguments.length === 0) {
+      return this.getCountries();
+    } else {
+      return fetch(this.API_SERVER + `/v3/covid-19/historical?lastdays=${lastdays}`, {
+        method: "GET"
+      })
+        .then(response => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.json();
+        })
+        .catch(error => console.log(error));
+    }
+  }
+
+  /**
+   * Get global accumulated COVID-19 time series data
+   * @param {string} lastdays - Number of days to return. Use 'all' for the full data set (e.g. 15, all, 24).
+   */
+  async getHistoricalAll(lastdays = 30) {
+    if (arguments.length === 0) {
+      return this.getCountries();
+    } else {
+      return fetch(this.API_SERVER + `/v3/covid-19/historical/all?lastdays=${lastdays}`, {
+        method: "GET"
+      })
+        .then(response => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.json();
+        })
+        .catch(error => console.log(error));
+    }
+  }
+  /**
+   * Get global accumulated COVID-19 time series data
+   * @param {string} country - A country name, iso2, iso3, or country ID code
+   * @param {string} lastdays - Number of days to return. Use 'all' for the full data set (e.g. 15, all, 24).
+   */
+  async getHistoricalCountry(country, lastdays = 30) {
+    if (arguments.length === 0) {
+      return this.getCountries();
+    } else {
+      return fetch(this.API_SERVER + `/v3/covid-19/historical/${country}?lastdays=${lastdays}`, {
+        method: "GET"
+      })
+        .then(response => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.json();
+        })
+        .catch(error => console.log(error));
+    }
+  }
 }
 
 export default Covid19API;
