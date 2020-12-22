b7b4872f20e88d38b6345d1045f39c2c108ca045
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2020-12-22 11:40:20 +0300

    fix: change color of map to less sad

--- covid-dashboard/src/WorldMap.js
+++ covid-dashboard/src/WorldMap.js
@@ -43,10 +43,12 @@ class WorldMap {
     init() {
         this.mymap = L.map(this.worldMap.id).setView([30.0, 60.0], 2);
         // Default style id: 'mapbox/streets-v11'
+        // Blue style id: 'nminkevich/ckiyzis0477jp1aqlqvezwx4l'
+        // Yellow id: 'nminkevich/ckiyzpcx00d9m19o3gghoyxeo'
         L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`, {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 13,
-            id: 'nminkevich/ckiyzis0477jp1aqlqvezwx4l',
+            id: 'nminkevich/ckiyzpcx00d9m19o3gghoyxeo',
             tileSize: 512,
             zoomOffset: -1,
             accessToken: 'pk.eyJ1Ijoibm1pbmtldmljaCIsImEiOiJjazVqeGVnYjEwN2trM29ybWtrdDBvOXFzIn0.0y70HLurlAEtyMY-ahO4CA'
